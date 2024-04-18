93f933d8061a1de9b5dd8fa1fb269576daf4706d
Tatyana Yusupova <tatyanayusupova17@gmail.com>
2024-04-18 15:02:03 +0200

    Optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -9,9 +9,9 @@ while True:
 
     if guess < number:
         print('Ваше число меньше того, что загадано.')
-    if guess > number:
+    elif guess > number:
         print('Ваше число больше того, что загадано.')
-    if guess == number:
+    elif guess == number:
         break                                          # Прерываем выполнение программы
 
 print('Отличная интуиция! Вы угадали число :)')
